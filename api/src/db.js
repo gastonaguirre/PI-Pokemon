7c74b88414f8655f7701b3062714d43cdb2ea8ab
gaston <ztonchyarg@hotmail.com>
2023-05-02 18:08:52 -0300

    Railway

--- api/src/db.js
+++ api/src/db.js
@@ -4,30 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const {PGDATABASE, PGHOST, PGPASSWORD , PGPORT , PGUSER} = process.env;
 
-let sequelize =
-  process.env.NODE_ENV === 'production'
-    ? new Sequelize({
-        database: PGDATABASE,
-        dialect: 'postgres',
-        host: PGHOST,
-        port: 5432,
-        username: PGUSER,
-        password: PGPASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false
-          },
-          keepAlive: true
-        },
-        ssl: true
-      })
-    : new Sequelize(`postgres://${ PGUSER }:${ PGPASSWORD }@${ PGHOST }:${ PGPORT }/${ PGDATABASE }`, {
+let sequelize = new Sequelize(`postgres://${ PGUSER }:${ PGPASSWORD }@${ PGHOST }:${ PGPORT }/${ PGDATABASE }`, {
         logging: false,
         native: false
       });
